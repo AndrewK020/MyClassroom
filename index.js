7e7beb208eecef317f47484505e055de64f9b575
Andrew <andrew.kelly20@protonmail.com>
2019-08-06 20:48:59 -0500

    setup heroku server

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 3001;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`🏹 Listening on port ${port}`));
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
