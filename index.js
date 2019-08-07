edfb751726ad5605ece0062ca42f8858191708d0
Andrew <andrew.kelly20@protonmail.com>
2019-08-06 20:31:39 -0500

    created react app with server start

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`🏹 Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
