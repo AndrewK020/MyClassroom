a2080faa4e61c2677a059e39dfab252e7e9bfa81
Andrew <andrew.kelly20@protonmail.com>
2019-08-08 19:08:00 -0500

    setup routes folder

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
+/* eslint-disable no-undef */
 const express = require('express');
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const app = express();
-const port = process.env.PORT || 3001;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`🏹 Listening on port ${port}`));
+const port = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes, both API and view
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/MyClassroom");
+
+app.listen(port, () => console.log(`🏹 Listening on port ${port}`));
