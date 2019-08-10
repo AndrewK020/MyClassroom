1350f0a1c1c5e29aa6752939b69506d2e4521a03
Andrew <andrew.kelly20@protonmail.com>
2019-08-10 12:35:23 -0500

    set up landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import ReactDOM from 'react-dom';
+// import Container from '@material-ui/core/Container'
+import Input from '@material-ui/core/Input'
+import Button from '@material-ui/core/Button';
+import logo from './apple.svg';
 import './App.css';
 
+
 class App extends Component {
-state = {
-    data: null
-  };
+  state = {
+      data: null
+    };
+
 
   componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+    console.log("Mounted!");
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
-
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="apple" />
+          <p>Enter Your Class Room Number</p>
+          <Input
+          placeholder="Room Number"
+          >
+          </Input>
+          <Button 
+          id="submitRoomBtn"
+          variant="contained">
+          Submit
+          </Button>
         </header>
-     { // Render the newly fetched data inside of this.state.data }
-     }
         <p className="App-intro">{this.state.data}</p>
       </div>
     );
